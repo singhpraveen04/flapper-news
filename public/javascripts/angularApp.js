5e2f5c34cfe105365ea1f3d7e26a9810d59611b2
Singh <praveen.e.singh@accenture.com>
2016-07-27 19:45:50 -0700

    initial commit comment

---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,69 @@
+(function(){
+var app = angular.module('flapperNews', ['ui.router']);
+
+app.factory('posts', function () {
+    var o = {
+        posts: []
+    };
+    return o;
+});
+
+app.controller('MainCtrl',  
+    function ($scope, posts) {
+        $scope.posts = posts.posts;
+
+        $scope.addPost = function () {
+            if (!$scope.title || $scope.title === '') {
+                return;
+            }
+        $scope.posts.push({
+            title: $scope.title,
+            link: $scope.link,
+            upvotes: 0,
+            comments: [
+                {author: 'Joe', body: 'cool post!', upvotes: 0},
+                {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+            ]
+        });
+        $scope.title = '';
+        $scope.link = '';
+    };
+
+    $scope.incrementUpvotes = function (post) {
+        post.upvotes += 1;
+    };
+});
+
+app.config([
+    '$stateProvider', 
+    '$urlRouterProvider', 
+    function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: '/home.html',
+            controller: 'MainCtrl'
+        })
+    .state('posts', {
+        url: '/posts/{id}',
+        templateUrl: '/posts.html',
+        controller: 'PostsCtrl'
+    });
+
+    $urlRouterProvider.otherwise('home');
+}]);
+    
+app.controller('PostsCtrl', 
+    function ($scope, $stateParams, posts){
+        $scope.post = posts.posts[$stateParams.id];
+        $scope.addComment = function(){
+            if($scope.body === ''){return;}
+            $scope.post.comments.push({
+                body: $scope.body,
+                author: 'user',
+                upvotes: 0
+            });
+        $scope.body = '';
+    };
+});
+})();
